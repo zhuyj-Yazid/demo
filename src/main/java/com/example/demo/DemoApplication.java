1b7819adcc8c694d80c77198d4c21d7fa0e92761
zhuyj <zhuyj@asiainfo.com>
2018-11-30 17:31:07 +0800

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -7,8 +9,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @SpringBootApplication
 public class DemoApplication {
-
+    private final static Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
+        LOGGER.info("DemoApplication start:"+ System.currentTimeMillis());
         SpringApplication.run(DemoApplication.class, args);
+        LOGGER.info("DemoApplication end:"+ System.currentTimeMillis());
     }
 }
